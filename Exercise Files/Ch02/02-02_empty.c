d95c0d53573d116f78ed440f5c0481b66b86f7a8
JoowonSuh <sjw2023@naver.com>
2021-02-02 18:06:11 -0800

    Practicing C with learning at LinkedIn

---
+++ Exercise Files/Ch02/02-02_empty.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	puts("This is my excercise");
+	puts("Thrilled to meet you!");
+
+	return(0);
+}
---
+++ Exercise Files/Ch02/02-07_twolines.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("first line\nsecond line\n");
+    return 0;
+}
